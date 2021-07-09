69d5f88552449711994db5b8b46b26f61547f761
hakdj <hakdjhakdj@naver.com>
2021-07-09 09:29:19 +0900

    0709_mapping_study

--- myanalysis/part6.py
+++ myanalysis/part6.py
@@ -7,12 +7,12 @@ import folium as fol;
 from config.settings import DATA_DIRS, STATICFILES_DIRS, TEMPLATES;
 
 df = pd.read_excel(DATA_DIRS[0]+'//data1.xlsx',engine='openpyxl',header=0);
-
+# 인구이동
 df2 = pd.read_excel(DATA_DIRS[0]+'//data2.xlsx',engine='openpyxl',header=0);
-
+# 전력량
 df3 = pd.read_csv(DATA_DIRS[0]+'//auto-mpg.csv',header=0);
 df3.columns= ['mpg','cyl','dis','hor','wei','acc','year','origin','name'];
-
+# auto
 
 df4 = pd.read_excel(DATA_DIRS[0]+'//data3.xlsx',engine='openpyxl');
 
@@ -23,11 +23,25 @@ df5 = pd.read_excel(DATA_DIRS[0]+'//data4.xlsx',engine='openpyxl');
 tt = sns.load_dataset('titanic');
 # titanic
 
-class Part5:
-    def p172(self):
-        print(tt.info());
+class Util:
+    def add10(self,n):
+        return n+10;
+    def add_two(self,a,b):
+        return a+b;
 
-
+class Part6:
+    def p218(self):
+        df=tt.loc[:,['age','fare']];
+        df['ten']=10;
+        print(df+100);
+        df2= df['age'].apply(Util().add10);
+        print(df2);
+        df3=df['age'].apply(Util().add_two,b=20);
+        print(df3);
+        df4=df['age'].apply(lambda x:Util().add10(x));
+        print(df4);
+        df5=df['age'].apply(lambda x:Util().add_two(20,x));
+        print(df5);
 
 if __name__ == '__main__':
-    Part5().p172();
+    Part6().p218();
